9ca5818a27d45e5ff4a13c5b7155e7953b8178f6
yoann <ygranier@gmail.com>
2017-04-05 19:24:38 +0200

    no message

---
+++ tankGameProject/Source/tankGameProject/tankGameProjectGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "tankGameProject.h"
+#include "tankGameProjectGameMode.h"
+
+
+
+
---
+++ tankGameProject/Source/tankGameProject/tankGameProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "tankGameProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAMEPROJECT_API AtankGameProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
